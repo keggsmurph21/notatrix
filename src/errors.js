dc8f4c2624e307860f84fcf4bfb496212b494e2b
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-06-03 23:04:13 -0300

    add error handling

---
+++ src/errors.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class NotatrixError extends Error {
+  constructor(...args) {
+    super(...args);
+  }
+}
+
+class InvalidCG3Error extends NotatrixError {
+  constructor(...args) {
+    super(...args);
+  }
+}
+
+class InvalidCoNLLUError extends NotatrixError {
+  constructor(...args) {
+    super(...args);
+  }
+}
+
+
+
+module.exports = {
+
+  NotatrixError: NotatrixError,
+  InvalidCG3Error: InvalidCG3Error,
+  InvalidCoNLLUError: InvalidCoNLLUError
+
+};
