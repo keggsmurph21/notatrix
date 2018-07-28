9a775a9bca2b09096d96a484f4046d9b4597493b
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-07-28 18:49:32 -0400

    implemented params and cg3 generators

--- src/formats/plain-text/generator.js
+++ src/formats/plain-text/generator.js
@@ -1,9 +1,30 @@
 'use strict';
 
+const _ = require('underscore');
+
 const utils = require('../../utils');
 const GeneratorError = utils.GeneratorError;
 
-module.exports = (text, options) => {
-  throw new GeneratorError('not implemented');
-};
+module.exports = (sent, options) => {
 
+  if (!sent || sent.name !== 'Sentence')
+    throw new GeneratorError(`Unable to generate, input not a Sentence`, sent, options);
+
+  options = _.extend(options, sent.options);
+  options = _.defaults(options, {
+
+  });
+
+  sent.index();
+  return sent.tokens.map(token => {
+
+    if (token.analysis && token.analyses.length > 1)
+      throw new GeneratorError('Unable to generate, contains ambiguous analyses');
+
+    if (token.isSuperToken) {
+      return token.subTokens.map(subToken => subToken.form).join(' ');
+    } else {
+      return token.form;
+    }
+  }).join(' ');
+};
