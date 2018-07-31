8bc5c7d4946d166c15d646dd69b6acf3b9638ecb
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-07-30 23:59:38 -0400

    implement the default splitter

--- src/splitter.js
+++ src/splitter.js
@@ -3,6 +3,8 @@
 const _ = require('underscore');
 
 const utils = require('./utils');
+const defaultSplitter = require('./formats/default-splitter');
+const detector = require('./detector');
 const SplitterError = utils.SplitterError;
 
 let as = {
@@ -28,8 +30,22 @@ let as = {
 
 module.exports = (text, options) => {
 
-  throw new SplitterError('not implemented');
+	let fromDefault = new Set();
+	const splitAsDefault = defaultSplitter(text, options);
+	splitAsDefault.forEach(line => {
+		detector(line, options).forEach(format => fromDefault.add(format));
+	});
 
+	let fromPlainText = new Set();
+	const splitAsPlainText = as.plainText(text, options);
+	splitAsPlainText.forEach(line => {
+		detector(line, options).forEach(format => fromPlainText.add(format));
+	});
+
+	if (fromPlainText.size === 1 && fromPlainText.has('plain text'))
+		return splitAsPlainText;
+
+	return splitAsDefault;
 };
 module.exports.as = as;
 module.exports.onNewlines = require('./formats/default-splitter');
