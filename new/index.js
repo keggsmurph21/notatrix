cc50d9706e2bffc921a3440c2a3b563cf804d838
Kevin Murphy <kmurphy4@swarthmore.edu>
2018-11-19 15:19:31 -0500

    first commit new branch

---
+++ new/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const Notatrix = require('./notatrix');
+
+n = new Notatrix('test');
---
+++ new/notatrix.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class Notatrix extends EventEmitter {
+  constructor(filename) {
+
+  }
+
+
+}
+
+module.exports = Notatrix;
